fae1ca1f89d1afad1086d6c722b4547f8beb7513
ujjawal291098 <ujjawalk.ic.17@nsit.net.in>
2025-10-25 00:39:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
